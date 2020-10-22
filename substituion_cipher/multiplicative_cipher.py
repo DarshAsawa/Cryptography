d8b97a8f51edbec8dcce41f9272096c8586a6026
Darsh Asawa <darshasawa@yahoo.co.in>
2020-10-22 10:37:49 +0530

    Multiplicative cipher in Substitution cipher

---
+++ substituion_cipher/multiplicative_cipher.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author: darshasawa
+"""
+
+# key domain for any multiplicative cipher : 1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25.
+
+key_domain = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+
+def caesar_cipher_encrypt(text,value):
+    cipher = ''
+    for char in text:
+        char = char.lower()
+        if char == ' ':
+            cipher+=char
+        else:
+            cipher += chr((((ord(char)-97) * value)%26)+97)
+    return cipher
+    
+
+text = input("Enter string: ")
+shift_value = int(input("Enter shift value: "))
+
+cipher_encrypted_text = caesar_cipher_encrypt(text,shift_value)
+print(cipher_encrypted_text)
